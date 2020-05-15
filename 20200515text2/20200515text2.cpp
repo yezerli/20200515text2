65fd8ec8de723d231a8c79cf6abca8524e5cb783
yezerli <1123603008@qq.com>
2020-05-15 15:18:04 +0800

    练习2

--- 20200515text2/20200515text2.cpp
+++ 20200515text2/20200515text2.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 #include<opencv.hpp>
-
 using namespace cv;
 using namespace std;
-
 
 int main()
 {
 	cv::Mat srcMat = imread("E:\\Git\\etest.jpg", 0);
 	cv::Mat dstMat;
-
 	if (srcMat.type() == CV_8UC1) {
 		equalizeHist(srcMat, dstMat);
 	}
@@ -17,17 +14,11 @@ int main()
 		Mat channel[3];
 		Mat out[3];
 		split(srcMat, channel);
-
 		for (int i = 0; i < 3; i++) {
 			equalizeHist(channel[i], out[i]);
 		}
 		merge(out, 3, dstMat);
 	}
-	float srcHist[256];
-	float dstHist[256];
-	cv::Mat srcHistMat;
-	cv::Mat dstHistMat;
-
 	imshow("srcMat", srcMat);
 	imshow("dstMat", dstMat);
 	waitKey(0);
